9cb8b3a7e05d807d308da649af4390a78079e39f
usmonjonqahhorov <qohhorovusmonjon@gmail.com>
2025-03-13 11:26:22 +0500

    docker configs

--- config/settings.py
+++ config/settings.py
@@ -115,8 +115,8 @@ DATABASES = {
         'NAME': os.environ.get('NAME_DB'),
         'USER': os.environ.get('USER_DB'),
         'PASSWORD': os.environ.get('PASSWORD_DB'),
-        'HOST': os.environ.get('HOST_DB'),
-        'PORT': os.environ.get('PORT_DB'),
+        'HOST': "admission-db",
+        'PORT': 5432,
     }
 }
 
