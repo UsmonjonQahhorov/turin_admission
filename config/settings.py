4918f2fc6d198353589297c089518aa05be363c7
usmonjonqahhorov <qohhorovusmonjon@gmail.com>
2025-03-27 20:45:20 +0500

    config and git ignore files

--- config/settings.py
+++ config/settings.py
@@ -195,8 +195,8 @@ SPECTACULAR_SETTINGS = {
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
-        'rest_framework.parsers.FormParser',  # Add this for form data
-        'rest_framework.parsers.MultiPartParser',  # Add this for file uploads
+        'rest_framework.parsers.FormParser',  
+        'rest_framework.parsers.MultiPartParser',  
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
